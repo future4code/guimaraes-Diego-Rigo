2d0c8a5237c877082985cf44c34f8442ada06134
darkrigo <diegorigo2@gmail.com>
2021-11-11 20:08:25 -0300

    exercícios-operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,93 @@
+//Exercícios de interpretação de código
+
+/*
+
+
+1
+
+a false
+b false
+c true
+d boolean
+
+2.
+// O problema é que ele esta tentando somar duas strings, pois o prompt retorna o valor como string.
+// será impresso 22
+
+
+3.
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+
+*/
+
+// ************************************************************************
+
+
+
+//Exercícios de escrita de código
+
+//****1.*****
+
+/*
+let idadeUsuario = Number(prompt("Qual é a sua idade?"))
+let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+
+let idadeMaior = idadeUsuario > idadeAmigo
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", '--', idadeMaior)
+
+console.log("diferença de idade é de", idadeUsuario - idadeAmigo, "anos")
+
+//**** 2. ****
+
+let numeroPar = Number(prompt("insira um numero Par:"))
+
+let restoDivisão = numeroPar % 2
+
+console.log(restoDivisão)
+
+//C - o padrão é que todos os numero pares retornam 0.
+
+//D - Se o numero digitado for impar, o resto retorna 1.
+
+
+
+//**** 3. ****
+
+let suaIdade = Number(prompt("Qual a sua idade?"))
+
+console.log("Sua idade em meses", "--",suaIdade * 12, "meses")
+console.log("Sua idade em dias", "--",suaIdade * 365, "dias")
+console.log("Sua idade em horas", "--",suaIdade * 365 * 24, "horas")
+
+*/
+
+//**** 4. ****
+
+let numeroUm = Number(prompt("Digite um numero:"))
+let numeroDois = Number(prompt("Digite outro numero"))
+
+
+console.log("O primeiro numero é maior que segundo?", "--", numeroUm > numeroDois)
+console.log("O primeiro numero é igual ao segundo?", "--", numeroUm === numeroDois)
+console.log("O primeiro numero é divisível pelo segundo?", "--", numeroUm % numeroDois === 0)
+console.log("O segundo numero é divisível pelo primeiro?", "--", numeroDois % numeroUm ===0)
+
+
+
+
+
+
+
+
+
+
+
+
+
