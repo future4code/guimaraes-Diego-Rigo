b291edd45ceb32777c54856ee37b5dfb4d87dc58
Your Name <diegorigo2@gmail.com>
2022-04-07 19:49:31 -0300

    colocando os elementos nas paginas

---
+++ modulo4/projeto-labex/projeto-labex/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+    return (
+      <div className="App">
+        <h1>Labex</h1>
+        <button>Ver Viagens</button>
+        <button>Administrativo</button>
+       
+      </div>
+    );
+  }
+  
+  export default HomePage;
---
+++ modulo4/projeto-labex/projeto-labex/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function LoginPage() {
+    return (
+      <div className="App">
+        <h1>Login</h1>
+        <input placeholder="E-mail"></input><br/>
+        <input placeholder="Senha"></input><br/>
+        <button>Voltar</button>
+        <button>Entrar</button>
+      </div>
+    );
+  }
+  
+  export default LoginPage;
