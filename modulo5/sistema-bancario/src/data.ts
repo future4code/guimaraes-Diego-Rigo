cc0f7665bf3ac42e62d4b84d0c5c12204f965011
Your Name <diegorigo2@gmail.com>
2022-06-13 17:38:34 -0300

    implementando data

---
+++ modulo5/sistema-bancario/src/data.ts
@@ -0,0 +1,13 @@
+export const listClient = [
+    {
+        id: '',
+        infoCliente: [
+        {
+            nome: '',
+            cpf:'',
+            nascimento:'',
+        }
+    ]
+    
+    }
+] 
--- modulo5/sistema-bancario/src/index.ts
+++ modulo5/sistema-bancario/src/index.ts
@@ -1,16 +1,44 @@
 import express from 'express'
 import { Request, Response } from 'express'
 import cors from 'cors'
+import { listClient } from './data'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-//exercicio 1
+
 app.get('/', (request: Request, response: Response) => {
     response.send('Pronto e operante');
 });
 
+//Criar Novo cliente
+app.post('/create', (request: Request, response: Response) => {
+    try{
+        const cliente = listClient.find((u) => u.id);
+
+        const name = request.body.name
+        const cpf = request.body.cpf
+        const nascimento = request.body.nascimento
+        
+        const newClient = {
+            id: "",
+            nome:name,
+            cpf: cpf,
+            data:nascimento
+        }
+    cliente.infoCliente.push(newClient);
+    response
+        .status(201)
+        .send(`${newClient.nome}criado com sucesso!`);
+
+    }catch (error:any){
+        response.end(error.message)
+    }
+
+
+
+});
 
 app.listen(3003, () => {
     console.log('Pronto e operante');
