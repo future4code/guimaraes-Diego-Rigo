a68e3a13b194164ab63722932cb5bf39fd4ba994
Your Name <diegorigo2@gmail.com>
2022-06-26 20:22:39 -0300

    subindo pr

---
+++ modulo5/toDolist/src/data.ts
@@ -0,0 +1,36 @@
+import connection from './connection';
+import { v4 as uuidv4 } from 'uuid';
+
+const populateUserTable = async (): Promise<void> => {
+    try {
+        await connection.raw(`
+            INSERT INTO TodoListUser (id, name, nickname, email) VALUES 
+            (
+                '${uuidv4()}',
+                'Astrodev',
+                'astrodev',
+                'astrodev@gmail.com'
+            ),
+            (
+                '${uuidv4()}',
+                'Aline Vignoli',
+                'nyhvignoli',
+                'nyhv@gmail.com'
+            ),
+            (
+                '${uuidv4()}',
+                'Dori',
+                'dori',
+                'dori@gmail.com'
+            );
+        `);
+
+        console.log('3 users inserted to get you started!');
+        connection.destroy();        
+    } catch (error:any) {
+        console.log(error.sqlMessage || error.message);
+    };
+};
+
+populateUserTable(); 
+ 38  
